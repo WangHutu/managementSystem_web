7c4013128563e709f8b0c517bb48fc9b8e1cd9df
runfengw <458473502@qq.com>
2023-02-21 23:26:46 +0800

    dialog

--- src/components/mainWrap.vue
+++ src/components/mainWrap.vue
@@ -1,300 +1,306 @@
 <script setup>
-import { ref } from 'vue'
-import { Search } from '@element-plus/icons-vue'
-import systable from './systable.vue'
-const selectType = ref([])
-const selectStatus = ref([])
-const searchIp = ref([])
+import { ref } from "vue";
+import { Search } from "@element-plus/icons-vue";
+import systable from "./systable.vue";
+import sysdialog from "./sysdialog.vue";
+const selectType = ref([]);
+const selectStatus = ref([]);
+const searchIp = ref([]);
+let dialogFormVisible = ref(false);
 const tableData = [
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: '',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "False",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: '',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "False",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: '',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "False",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: '',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "False",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: '',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "False",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: '',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "False",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: '',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "False",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: '',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "False",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: '',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "",
     status: "Ture",
-    remark: "备注"
+    remark: "备注",
   },
   {
-    type: 'kv260',
-    ip: '10.176.179.103',
-    user: 'runfengw',
+    type: "kv260",
+    ip: "10.176.179.103",
+    user: "runfengw",
     status: "False",
-    remark: "备注"
+    remark: "备注",
   },
-
-]
+];
 const types = [
   {
-    value: 'kv260',
-    label: 'kv260'
+    value: "kv260",
+    label: "kv260",
   },
   {
-    value: 'vck190',
-    label: 'vck190'
+    value: "vck190",
+    label: "vck190",
   },
   {
-    value: 'vek280',
-    label: 'vek280'
+    value: "vek280",
+    label: "vek280",
   },
   {
-    value: 'zcu104',
-    label: 'zcu104'
+    value: "zcu104",
+    label: "zcu104",
   },
   {
-    value: 'zcu102',
-    label: 'zcu102'
+    value: "zcu102",
+    label: "zcu102",
   },
-]
+];
 const status = [
   {
-    value: 'vacant',
-    label: 'vacant'
+    value: "vacant",
+    label: "vacant",
   },
   {
-    value: 'occupy',
-    label: 'occupy'
-  }
-]
-function searchhandle(val) {
-  console.log(val)
-}
+    value: "occupy",
+    label: "occupy",
+  },
+];
+const searchhandle = (val) => {
+  console.log(val);
+};
+const showDialog = () => {
+  // console.log(that);
+  dialogFormVisible = true;
+  console.log(dialogFormVisible);
+};
 </script>
 
 <template>
@@ -302,23 +308,51 @@ function searchhandle(val) {
     <div class="main">
       <div class="main_head">
         <el-row>
-          <el-col :span="12" style="padding-right: 20px;">
-            <el-select v-model="selectType" filterable multiple placeholder="Select Type" style="width:100%">
-              <el-option v-for="item in types" :key="item.value" :label="item.label" :value="item.value" />
+          <el-col :span="12" style="padding-right: 20px">
+            <el-select
+              v-model="selectType"
+              filterable
+              multiple
+              placeholder="Select Type"
+              style="width: 100%"
+            >
+              <el-option
+                v-for="item in types"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
             </el-select>
           </el-col>
-          <el-col :span="5" style="padding-right: 20px;"><el-select v-model="selectStatus" clearable
-              placeholder="Select Status" style="width:100%">
-              <el-option v-for="item in status" :key="item.value" :label="item.label" :value="item.value" />
-            </el-select></el-col>
-          <el-col :span="5" style="padding-right: 10px;"><el-input v-model="searchIp" :suffix-icon="Search"
-              placeholder="Enter ip to search" clearable @input="searchhandle" /></el-col>
-          <el-col :span="2" style="text-align:right">
-            <el-button type="primary">ADD</el-button>
+          <el-col :span="5" style="padding-right: 20px"
+            ><el-select
+              v-model="selectStatus"
+              clearable
+              placeholder="Select Status"
+              style="width: 100%"
+            >
+              <el-option
+                v-for="item in status"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              /> </el-select
+          ></el-col>
+          <el-col :span="5" style="padding-right: 10px"
+            ><el-input
+              v-model="searchIp"
+              :suffix-icon="Search"
+              placeholder="Enter ip to search"
+              clearable
+              @input="searchhandle"
+          /></el-col>
+          <el-col :span="2" style="text-align: right">
+            <el-button type="primary" @click="showDialog">ADD</el-button>
           </el-col>
         </el-row>
       </div>
       <systable :tableData="tableData"></systable>
+      <sysdialog :dialogFormVisible="dialogFormVisible"></sysdialog>
     </div>
   </el-scrollbar>
 </template>
---
+++ src/components/sysdialog.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-dialog v-model="dialogFormVisibleUpdate" title="Shipping address">
+<!--    <el-form :model="form">-->
+<!--      <el-form-item label="Promotion name" :label-width="formLabelWidth">-->
+<!--        <el-input v-model="form.name" autocomplete="off" />-->
+<!--      </el-form-item>-->
+<!--      <el-form-item label="Zones" :label-width="formLabelWidth">-->
+<!--        <el-select v-model="form.region" placeholder="Please select a zone">-->
+<!--          <el-option label="Zone No.1" value="shanghai" />-->
+<!--          <el-option label="Zone No.2" value="beijing" />-->
+<!--        </el-select>-->
+<!--      </el-form-item>-->
+<!--    </el-form>-->
+<!--    <template #footer>-->
+<!--      <span class="dialog-footer">-->
+<!--        <el-button @click="dialogFormVisible = false">Cancel</el-button>-->
+<!--        <el-button type="primary" @click="onClose"> Confirm </el-button>-->
+<!--      </span>-->
+<!--    </template>-->
+  </el-dialog>
+</template>
+
+<script setup>
+import {computed, reactive, toRef} from "vue";
+
+const props = defineProps({
+  dialogFormVisible: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+console.log(props.dialogFormVisible);
+
+const formLabelWidth = "140px";
+const emits = defineEmits(["update:dialogFormVisible"]);
+const dialogFormVisibleUpdate = computed(() => props.dialogFormVisible)
+console.log(dialogFormVisibleUpdate);
+
+const onClose = () => {
+};
+</script>
+
+<style scoped></style>
